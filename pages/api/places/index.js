bc359bd3d9e4fc50b7dc2b7968384d7a2083fb51
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-24 15:03:11 +0200

    create done

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,10 +5,27 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const places = await Place.find();
-    console.log(places);
-    return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    try {
+      const places = await Place.find();
+      console.log(places);
+      return response.status(200).json(places);
+    } catch (error) {
+      return response.status(405).json({ message: "Method not allowed" });
+    }
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+
+      const place = new Place(placeData);
+
+      await place.save();
+
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
 }
